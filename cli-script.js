8ea3433bbb6c08eba60f2fc4021544a88fad9cc8
Marek <marekz@gmail.com>
2017-10-19 10:14:02 -0400

    Add very simple CLI script

---
+++ cli-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const meow = require('meow');
+const renderdoc = require('.');
+
+const cli = meow('Usage: renderdoc <path> [options...]');
+
+const options = cli.flags;
+if (cli.input[0]) {
+  options.sourceDir = cli.input[0];
+}
+renderdoc.buildSite(options);
