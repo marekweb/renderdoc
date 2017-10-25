bb4e3650fa2563d0847d7eb66dadde072e6afbba
Marek <marekz@gmail.com>
2017-10-25 12:41:03 -0400

    Add NODE_ENV to silence React warnings

--- cli-script.js
+++ cli-script.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+// In production mode, React does not emit warnings. This should be safe to do
+// in CLI mode, but in other cases when the module is included in another
+// project then messing with NODE_ENV should be avoided.
+process.env.NODE_ENV = 'production';
+
 const meow = require('meow');
 const renderdoc = require('.');
 
